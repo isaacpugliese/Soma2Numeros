284eb492c75d5a7e547f53e874b150a624cbae12
Isaac Pugliese <isaacpugliese2010@gmail.com>
2020-09-26 15:08:51 -0300

    soma de 2 numeros

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace soma2numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1, numero2;
+            int resposta;
+
+            Console.Write("primeiro valor: ");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("segundo valor: ");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            resposta = numero1 + numero2;
+
+            Console.WriteLine($"A soma dos numeros é {resposta}");
+        }
+    }
+}
